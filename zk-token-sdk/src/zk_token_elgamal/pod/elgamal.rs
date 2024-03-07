6511cfa078c164c19c053bb070d7a58c77244f4f
samkim-crypto <skim13@cs.stanford.edu>
2024-03-07 17:40:55 +0900

    implement `FromStr` for `ElGamalPubkey` and `ElGamalCiphertext`

--- zk-token-sdk/src/zk_token_elgamal/pod/elgamal.rs
+++ zk-token-sdk/src/zk_token_elgamal/pod/elgamal.rs
@@ -7,21 +7,27 @@ use {
 };
 use {
     crate::{
-        zk_token_elgamal::pod::{pedersen::PEDERSEN_COMMITMENT_LEN, Pod, Zeroable},
+        zk_token_elgamal::pod::{pedersen::PEDERSEN_COMMITMENT_LEN, ParseError, Pod, Zeroable},
         RISTRETTO_POINT_LEN,
     },
     base64::{prelude::BASE64_STANDARD, Engine},
-    std::fmt,
+    std::{fmt, str::FromStr},
 };
 
 /// Byte length of an ElGamal public key
 const ELGAMAL_PUBKEY_LEN: usize = RISTRETTO_POINT_LEN;
+
+/// Maximum length of a base64 encoded ElGamal public key
+const ELGAMAL_PUBKEY_MAX_BASE64_LEN: usize = 44;
 
 /// Byte length of a decrypt handle
 pub(crate) const DECRYPT_HANDLE_LEN: usize = RISTRETTO_POINT_LEN;
 
 /// Byte length of an ElGamal ciphertext
 const ELGAMAL_CIPHERTEXT_LEN: usize = PEDERSEN_COMMITMENT_LEN + DECRYPT_HANDLE_LEN;
+
+/// Maximum length of a base64 encoded ElGamal ciphertext
+const ELGAMAL_CIPHERTEXT_MAX_BASE64_LEN: usize = 88;
 
 /// The `ElGamalCiphertext` type as a `Pod`.
 #[derive(Clone, Copy, Pod, Zeroable, PartialEq, Eq)]
@@ -43,6 +49,24 @@ impl fmt::Display for ElGamalCiphertext {
 impl Default for ElGamalCiphertext {
     fn default() -> Self {
         Self::zeroed()
+    }
+}
+
+impl FromStr for ElGamalCiphertext {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() > ELGAMAL_CIPHERTEXT_MAX_BASE64_LEN {
+            return Err(ParseError::WrongSize);
+        }
+        let ciphertext_vec = BASE64_STANDARD.decode(s).map_err(|_| ParseError::Invalid)?;
+        if ciphertext_vec.len() != std::mem::size_of::<ElGamalCiphertext>() {
+            Err(ParseError::WrongSize)
+        } else {
+            <[u8; ELGAMAL_CIPHERTEXT_LEN]>::try_from(ciphertext_vec)
+                .map_err(|_| ParseError::Invalid)
+                .map(ElGamalCiphertext)
+        }
     }
 }
 
@@ -76,6 +100,24 @@ impl fmt::Debug for ElGamalPubkey {
 impl fmt::Display for ElGamalPubkey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", BASE64_STANDARD.encode(self.0))
+    }
+}
+
+impl FromStr for ElGamalPubkey {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.len() > ELGAMAL_PUBKEY_MAX_BASE64_LEN {
+            return Err(ParseError::WrongSize);
+        }
+        let pubkey_vec = BASE64_STANDARD.decode(s).map_err(|_| ParseError::Invalid)?;
+        if pubkey_vec.len() != std::mem::size_of::<ElGamalPubkey>() {
+            Err(ParseError::WrongSize)
+        } else {
+            <[u8; ELGAMAL_PUBKEY_LEN]>::try_from(pubkey_vec)
+                .map_err(|_| ParseError::Invalid)
+                .map(ElGamalPubkey)
+        }
     }
 }
 
@@ -127,5 +169,34 @@ impl TryFrom<DecryptHandle> for decoded::DecryptHandle {
 
     fn try_from(pod_handle: DecryptHandle) -> Result<Self, Self::Error> {
         Self::from_bytes(&pod_handle.0).ok_or(ElGamalError::CiphertextDeserialization)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {super::*, crate::encryption::elgamal::ElGamalKeypair};
+
+    #[test]
+    fn elgamal_pubkey_fromstr() {
+        let elgamal_keypair = ElGamalKeypair::new_rand();
+        let expected_elgamal_pubkey: ElGamalPubkey = (*elgamal_keypair.pubkey()).into();
+
+        let elgamal_pubkey_base64_str = format!("{}", expected_elgamal_pubkey);
+        let computed_elgamal_pubkey = ElGamalPubkey::from_str(&elgamal_pubkey_base64_str).unwrap();
+
+        assert_eq!(expected_elgamal_pubkey, computed_elgamal_pubkey);
+    }
+
+    #[test]
+    fn elgamal_ciphertext_fromstr() {
+        let elgamal_keypair = ElGamalKeypair::new_rand();
+        let expected_elgamal_ciphertext: ElGamalCiphertext =
+            elgamal_keypair.pubkey().encrypt(0_u64).into();
+
+        let elgamal_ciphertext_base64_str = format!("{}", expected_elgamal_ciphertext);
+        let computed_elgamal_ciphertext =
+            ElGamalCiphertext::from_str(&elgamal_ciphertext_base64_str).unwrap();
+
+        assert_eq!(expected_elgamal_ciphertext, computed_elgamal_ciphertext);
     }
 }
