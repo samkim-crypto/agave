7e678769138bcddfafa674d7506b4c5016ab683c
samkim-crypto <skim13@cs.stanford.edu>
2024-04-19 08:02:10 +0900

    add instruction data for ciphertext with 3 handles validity proof

--- zk-token-sdk/src/instruction/grouped_ciphertext_validity/mod.rs
+++ zk-token-sdk/src/instruction/grouped_ciphertext_validity/mod.rs
@@ -1,5 +1,9 @@
 mod handles_2;
+mod handles_3;
 
 pub use handles_2::{
     GroupedCiphertext2HandlesValidityProofContext, GroupedCiphertext2HandlesValidityProofData,
+};
+pub use handles_3::{
+    GroupedCiphertext3HandlesValidityProofContext, GroupedCiphertext3HandlesValidityProofData,
 };
