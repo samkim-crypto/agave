1b45fa236e62e8a150f83799db909a8d807499fb
samkim-crypto <skim13@cs.stanford.edu>
2024-04-19 10:41:56 +0900

    cargo fmt

--- zk-token-sdk/src/instruction/grouped_ciphertext_validity/mod.rs
+++ zk-token-sdk/src/instruction/grouped_ciphertext_validity/mod.rs
@@ -1,9 +1,11 @@
 mod handles_2;
 mod handles_3;
 
-pub use handles_2::{
-    GroupedCiphertext2HandlesValidityProofContext, GroupedCiphertext2HandlesValidityProofData,
-};
-pub use handles_3::{
-    GroupedCiphertext3HandlesValidityProofContext, GroupedCiphertext3HandlesValidityProofData,
+pub use {
+    handles_2::{
+        GroupedCiphertext2HandlesValidityProofContext, GroupedCiphertext2HandlesValidityProofData,
+    },
+    handles_3::{
+        GroupedCiphertext3HandlesValidityProofContext, GroupedCiphertext3HandlesValidityProofData,
+    },
 };
