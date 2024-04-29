6e3e4b74f54a73150af30cda8b5be2437d01698e
samkim-crypto <skim13@cs.stanford.edu>
2024-04-29 17:09:35 +0900

    add simd number

--- sdk/src/feature_set.rs
+++ sdk/src/feature_set.rs
@@ -995,7 +995,7 @@ lazy_static! {
         (enable_tower_sync_ix::id(), "Enable tower sync vote instruction"),
         (chained_merkle_conflict_duplicate_proofs::id(), "generate duplicate proofs for chained merkle root conflicts"),
         (reward_full_priority_fee::id(), "Reward full priority fee to validators #34731"),
-        (abort_on_invalid_curve::id(), "Abort when elliptic curve syscalls invoked on invalid curve id")
+        (abort_on_invalid_curve::id(), "Abort when elliptic curve syscalls invoked on invalid curve id SIMD-0137")
         /*************** ADD NEW FEATURES HERE ***************/
     ]
     .iter()
