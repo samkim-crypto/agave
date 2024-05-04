b47f04c7a1f2900cf59c809676a8b2774fb3e83b
samkim-crypto <skim13@cs.stanford.edu>
2024-05-04 17:53:07 +0900

    add `pod::elgamal`

--- zk-sdk/src/encryption/pod/elgamal.rs
+++ zk-sdk/src/encryption/pod/elgamal.rs
@@ -1,33 +1,25 @@
 //! Plain Old Data types for the ElGamal encryption scheme.
 
+use {
+    crate::encryption::{
+        elgamal::{DECRYPT_HANDLE_LEN, ELGAMAL_CIPHERTEXT_LEN, ELGAMAL_PUBKEY_LEN},
+        pod::impl_from_str,
+    },
+    base64::{prelude::BASE64_STANDARD, Engine},
+    bytemuck::{Pod, Zeroable},
+    std::fmt,
+};
 #[cfg(not(target_os = "solana"))]
 use {
     crate::{
-        encryption::elgamal::{self as decoded},
+        encryption::elgamal::{DecryptHandle, ElGamalCiphertext, ElGamalPubkey},
         errors::ElGamalError,
     },
     curve25519_dalek::ristretto::CompressedRistretto,
 };
-use {
-    crate::{
-        zk_token_elgamal::pod::{impl_from_str, pedersen::PEDERSEN_COMMITMENT_LEN, Pod, Zeroable},
-        RISTRETTO_POINT_LEN,
-    },
-    base64::{prelude::BASE64_STANDARD, Engine},
-    std::fmt,
-};
-
-/// Byte length of an ElGamal public key
-const ELGAMAL_PUBKEY_LEN: usize = RISTRETTO_POINT_LEN;
 
 /// Maximum length of a base64 encoded ElGamal public key
 const ELGAMAL_PUBKEY_MAX_BASE64_LEN: usize = 44;
-
-/// Byte length of a decrypt handle
-pub(crate) const DECRYPT_HANDLE_LEN: usize = RISTRETTO_POINT_LEN;
-
-/// Byte length of an ElGamal ciphertext
-pub(crate) const ELGAMAL_CIPHERTEXT_LEN: usize = PEDERSEN_COMMITMENT_LEN + DECRYPT_HANDLE_LEN;
 
 /// Maximum length of a base64 encoded ElGamal ciphertext
 const ELGAMAL_CIPHERTEXT_MAX_BASE64_LEN: usize = 88;
@@ -35,44 +27,44 @@ const ELGAMAL_CIPHERTEXT_MAX_BASE64_LEN: usize = 88;
 /// The `ElGamalCiphertext` type as a `Pod`.
 #[derive(Clone, Copy, Pod, Zeroable, PartialEq, Eq)]
 #[repr(transparent)]
-pub struct ElGamalCiphertext(pub [u8; ELGAMAL_CIPHERTEXT_LEN]);
+pub struct PodElGamalCiphertext(pub(crate) [u8; ELGAMAL_CIPHERTEXT_LEN]);
 
-impl fmt::Debug for ElGamalCiphertext {
+impl fmt::Debug for PodElGamalCiphertext {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self.0)
     }
 }
 
-impl fmt::Display for ElGamalCiphertext {
+impl fmt::Display for PodElGamalCiphertext {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", BASE64_STANDARD.encode(self.0))
     }
 }
 
-impl Default for ElGamalCiphertext {
+impl Default for PodElGamalCiphertext {
     fn default() -> Self {
         Self::zeroed()
     }
 }
 
 impl_from_str!(
-    TYPE = ElGamalCiphertext,
+    TYPE = PodElGamalCiphertext,
     BYTES_LEN = ELGAMAL_CIPHERTEXT_LEN,
     BASE64_LEN = ELGAMAL_CIPHERTEXT_MAX_BASE64_LEN
 );
 
 #[cfg(not(target_os = "solana"))]
-impl From<decoded::ElGamalCiphertext> for ElGamalCiphertext {
-    fn from(decoded_ciphertext: decoded::ElGamalCiphertext) -> Self {
+impl From<ElGamalCiphertext> for PodElGamalCiphertext {
+    fn from(decoded_ciphertext: ElGamalCiphertext) -> Self {
         Self(decoded_ciphertext.to_bytes())
     }
 }
 
 #[cfg(not(target_os = "solana"))]
-impl TryFrom<ElGamalCiphertext> for decoded::ElGamalCiphertext {
+impl TryFrom<PodElGamalCiphertext> for ElGamalCiphertext {
     type Error = ElGamalError;
 
-    fn try_from(pod_ciphertext: ElGamalCiphertext) -> Result<Self, Self::Error> {
+    fn try_from(pod_ciphertext: PodElGamalCiphertext) -> Result<Self, Self::Error> {
         Self::from_bytes(&pod_ciphertext.0).ok_or(ElGamalError::CiphertextDeserialization)
     }
 }
@@ -80,38 +72,38 @@ impl TryFrom<ElGamalCiphertext> for decoded::ElGamalCiphertext {
 /// The `ElGamalPubkey` type as a `Pod`.
 #[derive(Clone, Copy, Default, Pod, Zeroable, PartialEq, Eq)]
 #[repr(transparent)]
-pub struct ElGamalPubkey(pub [u8; ELGAMAL_PUBKEY_LEN]);
+pub struct PodElGamalPubkey(pub(crate) [u8; ELGAMAL_PUBKEY_LEN]);
 
-impl fmt::Debug for ElGamalPubkey {
+impl fmt::Debug for PodElGamalPubkey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self.0)
     }
 }
 
-impl fmt::Display for ElGamalPubkey {
+impl fmt::Display for PodElGamalPubkey {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{}", BASE64_STANDARD.encode(self.0))
     }
 }
 
 impl_from_str!(
-    TYPE = ElGamalPubkey,
+    TYPE = PodElGamalPubkey,
     BYTES_LEN = ELGAMAL_PUBKEY_LEN,
     BASE64_LEN = ELGAMAL_PUBKEY_MAX_BASE64_LEN
 );
 
 #[cfg(not(target_os = "solana"))]
-impl From<decoded::ElGamalPubkey> for ElGamalPubkey {
-    fn from(decoded_pubkey: decoded::ElGamalPubkey) -> Self {
+impl From<ElGamalPubkey> for PodElGamalPubkey {
+    fn from(decoded_pubkey: ElGamalPubkey) -> Self {
         Self(decoded_pubkey.into())
     }
 }
 
 #[cfg(not(target_os = "solana"))]
-impl TryFrom<ElGamalPubkey> for decoded::ElGamalPubkey {
+impl TryFrom<PodElGamalPubkey> for ElGamalPubkey {
     type Error = ElGamalError;
 
-    fn try_from(pod_pubkey: ElGamalPubkey) -> Result<Self, Self::Error> {
+    fn try_from(pod_pubkey: PodElGamalPubkey) -> Result<Self, Self::Error> {
         Self::try_from(pod_pubkey.0.as_slice())
     }
 }
@@ -119,34 +111,34 @@ impl TryFrom<ElGamalPubkey> for decoded::ElGamalPubkey {
 /// The `DecryptHandle` type as a `Pod`.
 #[derive(Clone, Copy, Default, Pod, Zeroable, PartialEq, Eq)]
 #[repr(transparent)]
-pub struct DecryptHandle(pub [u8; DECRYPT_HANDLE_LEN]);
+pub struct PodDecryptHandle(pub(crate) [u8; DECRYPT_HANDLE_LEN]);
 
-impl fmt::Debug for DecryptHandle {
+impl fmt::Debug for PodDecryptHandle {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self.0)
     }
 }
 
 #[cfg(not(target_os = "solana"))]
-impl From<decoded::DecryptHandle> for DecryptHandle {
-    fn from(decoded_handle: decoded::DecryptHandle) -> Self {
+impl From<DecryptHandle> for PodDecryptHandle {
+    fn from(decoded_handle: DecryptHandle) -> Self {
         Self(decoded_handle.to_bytes())
     }
 }
 
 // For proof verification, interpret pod::DecryptHandle as CompressedRistretto
 #[cfg(not(target_os = "solana"))]
-impl From<DecryptHandle> for CompressedRistretto {
-    fn from(pod_handle: DecryptHandle) -> Self {
+impl From<PodDecryptHandle> for CompressedRistretto {
+    fn from(pod_handle: PodDecryptHandle) -> Self {
         Self(pod_handle.0)
     }
 }
 
 #[cfg(not(target_os = "solana"))]
-impl TryFrom<DecryptHandle> for decoded::DecryptHandle {
+impl TryFrom<PodDecryptHandle> for DecryptHandle {
     type Error = ElGamalError;
 
-    fn try_from(pod_handle: DecryptHandle) -> Result<Self, Self::Error> {
+    fn try_from(pod_handle: PodDecryptHandle) -> Result<Self, Self::Error> {
         Self::from_bytes(&pod_handle.0).ok_or(ElGamalError::CiphertextDeserialization)
     }
 }
@@ -158,10 +150,11 @@ mod tests {
     #[test]
     fn elgamal_pubkey_fromstr() {
         let elgamal_keypair = ElGamalKeypair::new_rand();
-        let expected_elgamal_pubkey: ElGamalPubkey = (*elgamal_keypair.pubkey()).into();
+        let expected_elgamal_pubkey: PodElGamalPubkey = (*elgamal_keypair.pubkey()).into();
 
         let elgamal_pubkey_base64_str = format!("{}", expected_elgamal_pubkey);
-        let computed_elgamal_pubkey = ElGamalPubkey::from_str(&elgamal_pubkey_base64_str).unwrap();
+        let computed_elgamal_pubkey =
+            PodElGamalPubkey::from_str(&elgamal_pubkey_base64_str).unwrap();
 
         assert_eq!(expected_elgamal_pubkey, computed_elgamal_pubkey);
     }
@@ -169,12 +162,12 @@ mod tests {
     #[test]
     fn elgamal_ciphertext_fromstr() {
         let elgamal_keypair = ElGamalKeypair::new_rand();
-        let expected_elgamal_ciphertext: ElGamalCiphertext =
+        let expected_elgamal_ciphertext: PodElGamalCiphertext =
             elgamal_keypair.pubkey().encrypt(0_u64).into();
 
         let elgamal_ciphertext_base64_str = format!("{}", expected_elgamal_ciphertext);
         let computed_elgamal_ciphertext =
-            ElGamalCiphertext::from_str(&elgamal_ciphertext_base64_str).unwrap();
+            PodElGamalCiphertext::from_str(&elgamal_ciphertext_base64_str).unwrap();
 
         assert_eq!(expected_elgamal_ciphertext, computed_elgamal_ciphertext);
     }
