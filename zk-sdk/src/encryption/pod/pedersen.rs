fa60ded2dce91ef8d05b2c9a9b319d84c76a3e56
samkim-crypto <skim13@cs.stanford.edu>
2024-05-04 17:52:59 +0900

    add `pod::pedersen`

--- zk-sdk/src/encryption/pod/pedersen.rs
+++ zk-sdk/src/encryption/pod/pedersen.rs
@@ -1,52 +1,47 @@
 //! Plain Old Data type for the Pedersen commitment scheme.
 
-#[cfg(not(target_os = "solana"))]
 use {
-    crate::{encryption::pedersen as decoded, errors::ElGamalError},
-    curve25519_dalek::ristretto::CompressedRistretto,
-};
-use {
-    crate::{
-        zk_token_elgamal::pod::{Pod, Zeroable},
-        RISTRETTO_POINT_LEN,
-    },
+    crate::encryption::pedersen::PEDERSEN_COMMITMENT_LEN,
+    bytemuck::{Pod, Zeroable},
     std::fmt,
 };
-
-/// Byte length of a Pedersen commitment
-pub(crate) const PEDERSEN_COMMITMENT_LEN: usize = RISTRETTO_POINT_LEN;
+#[cfg(not(target_os = "solana"))]
+use {
+    crate::{encryption::pedersen::PedersenCommitment, errors::ElGamalError},
+    curve25519_dalek::ristretto::CompressedRistretto,
+};
 
 /// The `PedersenCommitment` type as a `Pod`.
 #[derive(Clone, Copy, Default, Pod, Zeroable, PartialEq, Eq)]
 #[repr(transparent)]
-pub struct PedersenCommitment(pub [u8; PEDERSEN_COMMITMENT_LEN]);
+pub struct PodPedersenCommitment(pub(crate) [u8; PEDERSEN_COMMITMENT_LEN]);
 
-impl fmt::Debug for PedersenCommitment {
+impl fmt::Debug for PodPedersenCommitment {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:?}", self.0)
     }
 }
 
 #[cfg(not(target_os = "solana"))]
-impl From<decoded::PedersenCommitment> for PedersenCommitment {
-    fn from(decoded_commitment: decoded::PedersenCommitment) -> Self {
+impl From<PedersenCommitment> for PodPedersenCommitment {
+    fn from(decoded_commitment: PedersenCommitment) -> Self {
         Self(decoded_commitment.to_bytes())
     }
 }
 
 // For proof verification, interpret pod::PedersenCommitment directly as CompressedRistretto
 #[cfg(not(target_os = "solana"))]
-impl From<PedersenCommitment> for CompressedRistretto {
-    fn from(pod_commitment: PedersenCommitment) -> Self {
+impl From<PodPedersenCommitment> for CompressedRistretto {
+    fn from(pod_commitment: PodPedersenCommitment) -> Self {
         Self(pod_commitment.0)
     }
 }
 
 #[cfg(not(target_os = "solana"))]
-impl TryFrom<PedersenCommitment> for decoded::PedersenCommitment {
+impl TryFrom<PodPedersenCommitment> for PedersenCommitment {
     type Error = ElGamalError;
 
-    fn try_from(pod_commitment: PedersenCommitment) -> Result<Self, Self::Error> {
+    fn try_from(pod_commitment: PodPedersenCommitment) -> Result<Self, Self::Error> {
         Self::from_bytes(&pod_commitment.0).ok_or(ElGamalError::CiphertextDeserialization)
     }
 }
