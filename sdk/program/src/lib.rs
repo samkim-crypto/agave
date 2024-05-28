2c82c76f7c3b54eebb15e4d2ec470a7408ca3215
samkim-crypto <skim13@cs.stanford.edu>
2024-05-28 10:48:42 +0900

    remove unnecessary dependencies from `solana-program`

--- sdk/program/src/lib.rs
+++ sdk/program/src/lib.rs
@@ -471,7 +471,6 @@ extern crate self as solana_program;
 
 pub mod account_info;
 pub mod address_lookup_table;
-pub mod alt_bn128;
 pub(crate) mod atomic_u64;
 pub mod big_mod_exp;
 pub mod blake3;
