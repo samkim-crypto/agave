f66dfb798421346e16255d514356a1b77d2508f1
samkim-crypto <skim13@cs.stanford.edu>
2024-07-15 18:12:26 +0900

    add range proof u128 test

--- dalek-test/src/main.rs
+++ dalek-test/src/main.rs
@@ -1,12 +1,13 @@
 use {
     solana_sdk::{
-        signature::{Keypair, Signature, Signer},
+        instruction::Instruction,
+        signature::{Keypair, Signer},
         transaction::Transaction,
     },
     solana_test_validator::{TestValidator, TestValidatorGenesis},
     solana_zk_token_sdk::{
-        encryption::pedersen::Pedersen, instruction::BatchedRangeProofU64Data,
-        zk_token_proof_instruction::verify_batched_verify_range_proof_u64,
+        encryption::pedersen::Pedersen, instruction::BatchedRangeProofU128Data,
+        zk_token_proof_instruction::verify_batched_verify_range_proof_u128,
     },
     std::sync::Arc,
 };
@@ -18,21 +19,7 @@ async fn main() {
     let payer: Arc<dyn Signer> = Arc::new(payer);
     let rpc_client = Arc::new(test_validator.get_async_rpc_client());
 
-    let amount_1 = 23_u64;
-    let amount_2 = 24_u64;
-
-    let (commitment_1, opening_1) = Pedersen::new(amount_1);
-    let (commitment_2, opening_2) = Pedersen::new(amount_2);
-
-    let proof_data = BatchedRangeProofU64Data::new(
-        vec![&commitment_1, &commitment_2],
-        vec![amount_1, amount_2],
-        vec![32, 32],
-        vec![&opening_1, &opening_2],
-    )
-    .unwrap();
-
-    let instructions = vec![verify_batched_verify_range_proof_u64(None, &proof_data)];
+    let instructions = vec![batched_range_proof_u128()];
     let transaction = Transaction::new_signed_with_payer(
         &instructions,
         Some(&payer.pubkey()),
@@ -45,6 +32,24 @@ async fn main() {
         .await
         .unwrap();
     println!("signature: {:?}", signature);
+}
+
+fn batched_range_proof_u128() -> Instruction {
+    let amount_1 = 23_u64;
+    let amount_2 = 24_u64;
+
+    let (commitment_1, opening_1) = Pedersen::new(amount_1);
+    let (commitment_2, opening_2) = Pedersen::new(amount_2);
+
+    let proof_data = BatchedRangeProofU128Data::new(
+        vec![&commitment_1, &commitment_2],
+        vec![amount_1, amount_2],
+        vec![64, 64],
+        vec![&opening_1, &opening_2],
+    )
+    .unwrap();
+
+    verify_batched_verify_range_proof_u128(None, &proof_data)
 }
 
 async fn new_validator_for_test() -> (TestValidator, Keypair) {
