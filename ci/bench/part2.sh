63cb88b068b02ec2807203586750bbda0c69ff05
samkim-crypto <skim13@cs.stanford.edu>
2025-03-27 22:22:56 +0900

    replace zk-token-proof benches with zk-elgamal-proof benches in the ci script

--- ci/bench/part2.sh
+++ ci/bench/part2.sh
@@ -27,4 +27,4 @@ _ cargo +"$rust_nightly" run --release --manifest-path banking-bench/Cargo.toml 
 _ cargo +"$rust_nightly" run --release --manifest-path accounts-bench/Cargo.toml ${V:+--verbose} -- --num_accounts 10000 --num_slots 4 | tee -a "$BENCH_FILE"
 
 # Run zk-token-proof benches.
-_ cargo +"$rust_nightly" bench --manifest-path programs/zk-token-proof/Cargo.toml ${V:+--verbose} | tee -a "$BENCH_FILE"
+_ cargo +"$rust_nightly" bench --manifest-path programs/zk-elgamal-proof/Cargo.toml ${V:+--verbose} | tee -a "$BENCH_FILE"
